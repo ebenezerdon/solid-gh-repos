ae2d0b8c09bd26be7b385d545d78719a382e69ad
ebenezerdon <ebenezerdonu@gmail.com>
2024-02-08 19:40:37 +0100

    add implementation for trending repos

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,8 @@
 import { Route, Routes } from 'solid-app-router'
-import { Component, createEffect, createSignal, lazy } from 'solid-js'
+import { Component, lazy } from 'solid-js'
 import Nav from './components/Nav'
 const Home = lazy(() => import('./pages/Home'))
 const SavedRepos = lazy(() => import('./pages/SavedRepos'))
-
-const [username, setUserName] = createSignal('ebenezerdon')
-const [repos, setRepos] = createSignal([])
-
-createEffect(async () => {
-  const res = await fetch(`https://api.github.com/users/${username()}/repos?sort=created`)
-  setRepos(await res.json())
-})
 
 const App: Component = () => {
   return (
@@ -24,5 +16,4 @@ const App: Component = () => {
   )
 }
 
-export { username, setUserName, repos }
 export default App
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,27 @@
-import { Component, For } from 'solid-js'
-import { repos, setUserName, username } from '../App'
+import { Component, For, Show, createResource, createSignal } from 'solid-js'
 import RepoCard, { Repo } from '../components/RepoCard'
 
 const Home: Component = () => {
-  const refetchWithUsername = (event: Event) => {
+  const date = new Date()
+  date.setDate(date.getDate() - 7)
+  const formattedDate = date.toISOString().split('T')[0]
+
+  const [username, setUserName] = createSignal('')
+  const baseUrl = 'https://api.github.com'
+
+  const fetchRepos = async (username: string) => {
+    const trendingReposUrl = `${baseUrl}/search/repositories?q=created:>${formattedDate}&sort=stars&order=desc&per_page=30`
+    const userReposUrl = `${baseUrl}/users/${username}/repos?sort=created`
+    const url = username ? userReposUrl : trendingReposUrl
+
+    const res = await fetch(url)
+    const data = await res.json()
+    return data.items || data
+  }
+
+  const [repos] = createResource(username, fetchRepos)
+
+  const refetchRepos = (event: Event) => {
     event.preventDefault()
     const usernameInput = document.querySelector('#usernameInput') as HTMLInputElement
     setUserName(usernameInput.value)
@@ -11,14 +29,21 @@ const Home: Component = () => {
 
   return (
     <div>
-      <form class="mb-3" onSubmit={(event) => refetchWithUsername(event)}>
-        <input type="text" class="p-1 align-middle" placeholder="Type username..." id="usernameInput" required />
+      <Show when={repos.loading}>
+        <p>Loading...</p>
+      </Show>
+
+      <form class="mb-3" onSubmit={(event) => refetchRepos(event)}>
+        <input type="text" class="p-1 align-middle" placeholder="Type username..." id="usernameInput" />
 
         <button class="btn btn-dark ms-3 w-auto" type="submit">
           Fetch
         </button>
       </form>
-      <h3>GitHub repos for {username()}</h3>
+
+      <Show when={username()} fallback={<h1>Most starred repos since {formattedDate}</h1>}>
+        <h3>GitHub repos for {username()}</h3>
+      </Show>
       <For each={repos()}>{(repo: Repo) => <RepoCard repo={repo} />}</For>
     </div>
   )
